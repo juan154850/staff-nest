11622928004678af5bfdd8719e4fced7770f6b2b
juanb <juan@weship.com>
2025-05-07 00:28:10 -0600

    Proteger las rutas con el token jwt y crear el decorador para las rutas publicas

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,23 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: 'APP_GUARD',
+      useClass: AuthGuard,
+    },
+  ],
   imports: [
     UserModule,
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
     }),
   ],
   exports: [AuthService],
